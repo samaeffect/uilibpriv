436bc826ad91cbe0f87563ef62b7a73da7398a54
samaeffect <jsstaywithme@gmail.com>
2025-12-23 03:18:52 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -106,7 +106,7 @@ function Luxt1.CreateWindow(libName, logoId)
 
     --
     libName = libName or "LuxtLib"
-    logoId = logoId or "rbxassetid://97891580"
+    logoId = "rbxassetid://97891580"
     --
 
     LuxtLib.Name = "LuxtLib"..libName
