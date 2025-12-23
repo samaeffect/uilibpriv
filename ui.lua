664f6481c5d93b4b920eb82ef5ed2f4071fb1cea
samaeffect <jsstaywithme@gmail.com>
2025-12-23 03:33:04 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -143,7 +143,7 @@ function Luxt1.CreateWindow(libName, 97891580)
     hubLogo.Position = UDim2.new(0.0567928664, 0, 0.0243411884, 0)
     hubLogo.Size = UDim2.new(0, 30, 0, 30)
     hubLogo.ZIndex = 2
-    hubLogo.Image = "rbxassetid://"..logoId
+    hubLogo.Image = "rbxassetid://97891580"
 
     MainCorner_2.CornerRadius = UDim.new(0, 999)
     MainCorner_2.Name = "MainCorner"
